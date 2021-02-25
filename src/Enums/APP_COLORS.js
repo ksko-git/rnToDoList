c6c5b1c5f44d522f70b33f0a6dc5c1a614503ba9
Ксения <56629590+ksko-git@users.noreply.github.com>
2021-02-25 14:14:10 +0700

    Initial commit

---
+++ src/Enums/APP_COLORS.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const APP_COLORS = {
+    amethyst: '#9966CC',
+    LightLavender: '#9966FF',
+    deepLavender: '#6600CC',
+    lavender: '#6633CC',
+    purple: '#663399',
+    loopTallinn: '#330033',
+    pastelPink: '#ffd1dc',
+    mountbattenPink: '#996699',
+}
